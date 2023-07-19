2cf65c1a773892ce0d3da31e4194438a06b1cc0a
jironoidev1 <104747182+jironoidev1@users.noreply.github.com>
2023-07-20 00:41:09 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ let isAlive = setInterval(function () {
   );
 
   // detect collision
-  if (cactusLeft < 100 && cactusLeft > 0 && dinoTop >= 295) {
+  if (cactusLeft < 80 && cactusLeft > 0 && dinoTop >= 295) {
     // collision
     GOver();
   }
