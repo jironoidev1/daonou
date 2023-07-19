594f6d182dd77ed22f38f1825762cf10f268fc6f
jironoidev1 <104747182+jironoidev1@users.noreply.github.com>
2023-07-20 00:27:04 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function GOver() {
+  alert('Game Over');
+}
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 250);
+  }
+}
+
+let isAlive = setInterval(function () {
+  // get current dino Y position
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+  // get current cactus X position
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  // detect collision
+  if (cactusLeft < 100 && cactusLeft > 0 && dinoTop >= 295) {
+    // collision
+    GOver();
+  }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
+
+document.addEventListener("click", function (event) {
+  jump();
+});
